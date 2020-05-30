28a7ca037c227e7d86e53041aa6e5de647c5a0f8
prakash_dotworld <prakash@dotworld.in>
2020-05-30 10:00:48 +0530

    logo added to user details

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Card, Tag, Divider, List, Avatar } from 'antd';
-import {PlusOutlined} from '@ant-design/icons';
+import { Card, Tag, Divider, Row, Col } from 'antd';
+import {PlusOutlined, ContactsOutlined, ClusterOutlined, HomeOutlined} from '@ant-design/icons';
 import SimpleTags from './SimpleTags'
 
 export default function UserCard(props){
@@ -13,26 +13,27 @@ export default function UserCard(props){
             <p>{props.signature}</p>
           </div>
           <Card bordered={false} style={{ width: '100%' }}>
-            <p>{props.title}</p>
-            <p>{props.group}</p>
-            <p>{props.province}{props.city}</p>
+            <p><span className="info-logo"><ContactsOutlined /></span>{props.title}</p>
+            <p><span className="info-logo"><ClusterOutlined /></span>{props.group}</p>
+            <p><span className="info-logo"><HomeOutlined /></span>{props.province}{props.city}</p>
           </Card>
           <Divider/>
           <div>
+          <div class="tagsTitle">标签</div>
             <SimpleTags tags={props.tags}/>
             <Tag className="site-tag-plus" >
               <PlusOutlined />
             </Tag>
           </div>  
         <Divider/>
-        {props.data && <List grid={{gutter: 0,}}
-          dataSource={props.data}
-          renderItem={item => (
-            <List.Item>
-              <List.Item.Meta avatar={<Avatar src={item.logo} />}
-                title={<a href="https://ant.design">{item.member}</a>}/>
-            </List.Item>)}/>
-            }    
+        {props.data &&  <Row gutter={[16,16]} >
+                {props.data.map(item=>
+                  <Col span={12}>
+                    <img className="row-img" src={item.logo} alt="logo"/>
+                    {item.member}
+                  </Col>)}
+                </Row>
+               }    
         </>
     )
 }
